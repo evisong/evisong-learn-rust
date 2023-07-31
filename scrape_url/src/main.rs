867bb65e76c93de92ae9061d4c0ba41b7b38fc8a
EviSong <evisong@live.com>
2023-07-31 16:21:26 +0800

    03: quiz2: scrape_url cli args

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
 
 fn main() {
-  let url = "https://www.rust-lang.org/";
-  let output = "target/rust.md";
+  let args: Vec<String> = std::env::args().collect();
+  let url = args.get(1).cloned().unwrap_or("https://www.rust-lang.org/".to_string());
+  let output = args.get(2).cloned().unwrap_or("target/rust.md".to_string());
 
   println!("Fetching url: {}", url);
   let body = reqwest::blocking::get(url).unwrap().text().unwrap();
@@ -10,6 +11,6 @@ fn main() {
   println!("converting html to markdown...");
   let md = html2md::parse_html(&body);
 
-  fs::write(output, md.as_bytes()).unwrap();
+  fs::write(output.clone(), md.as_bytes()).unwrap();
   println!("Converted markdown has been saved to {}", output);
 }
